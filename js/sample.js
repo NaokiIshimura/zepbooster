441f0ec2e17ed580c7cdd2fae8670ea42338b103
Naoki Ishimura <mirontest1460@gmail.com>
2018-08-08 23:06:12 +0900

    first commit

---
+++ js/sample.js
@@ -0,0 +1,50 @@
+function convertStyleCss(){
+  baseWidth = document.getElementById('baseWidth').value;
+
+  // ??pxを ?? / 375 * 100vw に置換する
+  str = document.getElementById('styleCssInput').value;
+
+  result = str.match(/\d+px/g);
+  resultText = str;
+
+  if(result != null){
+    for (var index = 0; index < result.length; index++)
+    {
+      //console.log("submatch " + index + ": " +  result[index]);
+      replaceText = 'calc('+result[index].replace('px','')+' / '+baseWidth+' * 100vw)'
+      resultText = resultText.replace(result[index], replaceText);
+    }
+  }
+
+  // marginのテンプレートを追加する
+  replaceText  = '  \n'
+  replaceText += '  /*margin: 0 0;*/\n'
+  replaceText += '  /*margin-top: 0;*/\n'
+  replaceText += '  /*margin-bottom: 0;*/\n'
+  replaceText += '  /*margin-left: 0;*/\n'
+  replaceText += '  /*margin-right: 0;*/\n'
+  replaceText += '}'
+  resultText = resultText.replace('}', replaceText);
+
+  // 変換後のテキストをテキストエリアに表示する
+  document.getElementById('styleCssOutput').value = resultText;
+
+}
+
+function copyStyleCss(){
+   // コピー対象をJavaScript上で変数として定義する
+  copyTarget = document.getElementById('styleCssOutput');
+  // コピー対象のテキストを選択する
+  copyTarget.select();
+  // 選択しているテキストをクリップボードにコピーする
+  document.execCommand("Copy");
+}
+
+function convertAndCopyStyleCss(){
+  convertStyleCss();
+  copyStyleCss();
+}
+
+function convertImageHtml(){
+
+}
